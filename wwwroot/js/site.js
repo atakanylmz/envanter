5f05314d5c947871fa5c37cfe2eebf653738745e
atakanylmz <atakanylmz1@hotmail.com>
2023-08-25 15:15:40 +0300

    rolleme tamamlandı

--- wwwroot/js/site.js
+++ wwwroot/js/site.js
@@ -2,3 +2,7 @@
 // for details on configuring this project to bundle and minify static web assets.
 
 // Write your JavaScript code.
+$(function () {
+    var kullanici_tablo = $('#kullaniciTablo').DataTable();
+
+})
